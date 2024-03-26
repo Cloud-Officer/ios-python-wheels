770d606209526b15ff93ab27fb6e29b9cccaba29
Yves Desgagné <yves@cloudofficer.ca>
2024-03-26 14:58:47 -0400

    Latest

--- setup.sh
+++ setup.sh
@@ -75,10 +75,10 @@ popd &>/dev/null
 # dependencies
 
 dependencies=(
-  "gcc"
-  "libomp"
-  "openblas"
-  "xxhash"
+   "gcc"
+   "libomp"
+   "openblas"
+   "xxhash"
 )
 
 echo "Installing dependencies..."
@@ -89,38 +89,37 @@ for dependency in "${dependencies[@]}"; do
   fi
 done
 
-cp "$(brew --prefix openblas)"/lib/*.dylib "${frameworks_dir}"
+#cp "$(brew --prefix openblas)"/lib/*.dylib "${frameworks_dir}"
 cp "$(brew --prefix libomp)"/lib/*.dylib "${frameworks_dir}"
-cp "$(brew --prefix gcc)"/lib/gcc/current/libgfortran*.dylib "${frameworks_dir}"
-cp "$(brew --prefix gcc)"/lib/gcc/current/libgomp*.dylib "${frameworks_dir}"
-cp "$(brew --prefix gcc)"/lib/gcc/current/libquadmath*.dylib "${frameworks_dir}"
-cp "$(brew --prefix gcc)"/lib/gcc/current/libgcc_s*.dylib "${frameworks_dir}"
+#cp "$(brew --prefix gcc)"/lib/gcc/current/libgfortran*.dylib "${frameworks_dir}"
+#cp "$(brew --prefix gcc)"/lib/gcc/current/libgomp*.dylib "${frameworks_dir}"
+#cp "$(brew --prefix gcc)"/lib/gcc/current/libquadmath*.dylib "${frameworks_dir}"
+#cp "$(brew --prefix gcc)"/lib/gcc/current/libgcc_s*.dylib "${frameworks_dir}"
 
 for library in "${frameworks_dir}"/*.dylib; do
   xcrun vtool -arch arm64 -set-build-version 2 "${minimum_os_version}" "${sdk_version}" -replace -output "${library}" "${library}" &>/dev/null
-
   loader_paths=$(otool -L "${library}" | grep -v : | grep -v /usr/ | grep -v /System/ | awk '{ print $1 }')
 
   if [ -n "${loader_paths}" ]; then
     for loader_path in ${loader_paths}; do
       echo "Patching loader path ${loader_path}..."
-      install_name_tool -change "${loader_path}" "./$(basename ${loader_path})" "${library}" &>/dev/null
+      install_name_tool -change "${loader_path}" "@loader_path/$(basename ${loader_path})" "${library}" &>/dev/null
     done
   fi
 done
 
 # openblas
 
-#lapack_version="1.4"
+lapack_version="1.4"
 
-#curl --silent --location "https://github.com/ColdGrub1384/lapack-ios/releases/download/v${lapack_version}/lapack-ios.zip" --output lapack-ios.zip
-#unzip -q lapack-ios.zip
-#mv lapack-ios/openblas.framework "${frameworks_dir}"
-#mv lapack-ios/lapack.framework "${frameworks_dir}/scipy-deps.framework"
-#mv lapack-ios/ios_flang_runtime.framework "${frameworks_dir}"
-#cp "${frameworks_dir}/openblas.framework/openblas" "${frameworks_dir}/libopenblas.dylib"
-#cp "${frameworks_dir}/ios_flang_runtime.framework/ios_flang_runtime" "${frameworks_dir}/libgfortran.dylib"
-#rm -rf __MACOSX lapack-ios lapack-ios.zip
+curl --silent --location "https://github.com/ColdGrub1384/lapack-ios/releases/download/v${lapack_version}/lapack-ios.zip" --output lapack-ios.zip
+unzip -q lapack-ios.zip
+mv lapack-ios/openblas.framework "${frameworks_dir}"
+mv lapack-ios/lapack.framework "${frameworks_dir}/scipy-deps.framework"
+mv lapack-ios/ios_flang_runtime.framework "${frameworks_dir}"
+cp "${frameworks_dir}/openblas.framework/openblas" "${frameworks_dir}/libopenblas.dylib"
+cp "${frameworks_dir}/ios_flang_runtime.framework/ios_flang_runtime" "${frameworks_dir}/libgfortran.dylib"
+rm -rf __MACOSX lapack-ios lapack-ios.zip
 
 # package wheels
 
